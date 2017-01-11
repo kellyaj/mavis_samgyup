93ad7aee6b3ce47765d67fd2234e1f973b8bcd66
Andrew Kelly <kellyaj@gmail.com>
2017-01-11 00:16:35 -0800

    proof of concept wip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  handleKeyPress(e) {
+    console.log(e.key)
+  }
+
   render() {
-    <div className="app-container">
-    </div>
+    return (
+      <div className="app-container">
+        Press the key
+        <input type="text" onKeyPress={this.handleKeyPress} />
+      </div>
+    )
   }
 }
+
+module.exports = App
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,37 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var bourbon = require('node-bourbon').includePaths;
-var neat = require('node-neat').includePaths[1];
+var path = require('path')
+var webpack = require('webpack')
 
-var config = module.exports = {
-  context: __dirname,
-  entry: './src/entry.js',
-  plugins: [
-    new ExtractTextPlugin('./build/styles.css')
-  ]
-};
+var BUILD_DIR = path.resolve(__dirname + '/build')
+var SRC_DIR = path.resolve(__dirname + '/src')
 
-config.output = {
-  path: path.join(__dirname, 'build'),
-  filename: 'bundle.js',
-  publicPath: './build',
-  devtoolModuleFilenameTemplate: '[resourcePath]',
-  devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]',
-};
-
-config.resolve = {
-  extensions: ['', '.js'],
-  alias: {
-    'babel-runtime/regenerator': require.resolve('babel-runtime/regenerator')
+module.exports = {
+  entry: SRC_DIR + '/entry.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/'
   },
-  modulesDirectories: [ 'node_modules' ]
-};
-
-config.module = {
-  loaders: [
-    {
-      test: /\.js$/,
-      include: path.resolve(__dirname, './src'),
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'react']
+  devtool: 'source-map',
+  devServer: {
+    inline: true,
+    contentBase: BUILD_DIR,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: SRC_DIR,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, './src/styles'),
+        loader: 'style!css!postcss',
       }
-    },
-    {
-      test: /\.css$/,
-      include: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
-      loader: ExtractTextPlugin.extract('style', 'css?-autoprefixer!postcss')
-    },
-    {
-      test: /\.scss$/,
-      include: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
-      loader: "style!css!sass?includePaths[]=" + bourbon + "&includePaths[]=" + neat
-    }
-  ]
+    ]
+  }
 }
-
