453448156d9865efc5070f81825d1b65416b87b2
Andrew Kelly <kellyaj@gmail.com>
2017-02-05 18:26:14 -0800

    adds a large number of challenges

--- src/challenges.js
+++ src/challenges.js
@@ -20,7 +20,63 @@ module.exports = {
       {
         content: "젓가락",
         english: "chopsticks"
-      }
+      },
+      {
+        content: "포크",
+        english: "fork"
+      },
+      {
+        content: "얼음물",
+        english: "ice water"
+      },
+      {
+        content: "맥주",
+        english: "beer"
+      },
+      {
+        content: "냅킨",
+        english: "napkin"
+      },
+      {
+        content: "김치",
+        english: "kimchi"
+      },
+      {
+        content: "술집",
+        english: "bar"
+      },
+      {
+        content: "홍대",
+        english: "Hongdae"
+      },
+      {
+        content: "소주",
+        english: "soju"
+      },
+      {
+        content: "치즈옥수수",
+        english: "cheese corn"
+      },
+      {
+        content: "케밥",
+        english: "kebab"
+      },
+      {
+        content: "피자",
+        english: "pizza"
+      },
+      {
+        content: "떡볶이",
+        english: "ddeokbokki"
+      },
+      {
+        content: "만두",
+        english: "mandu"
+      },
+      {
+        content: "비빔밥",
+        english: "bibimbap"
+      },
     ]
   },
   "SCHOOL": {
@@ -28,6 +84,10 @@ module.exports = {
     difficulty: EASY_DIFFICULTY,
     iconClass: "graduation-cap",
     challenges: [
+      {
+        content: "책",
+        english: "book"
+      },
       {
         content: "책상",
         english: "desk"
@@ -39,7 +99,55 @@ module.exports = {
       {
         content: "선생님",
         english: "teacher"
-      }
+      },
+      {
+        content: "학원",
+        english: "private academy (hagwon)"
+      },
+      {
+        content: "공립학교",
+        english: "public school"
+      },
+      {
+        content: "수학",
+        english: "math"
+      },
+      {
+        content: "과학",
+        english: "science"
+      },
+      {
+        content: "영어",
+        english: "english"
+      },
+      {
+        content: "문학",
+        english: "literature"
+      },
+      {
+        content: "음악",
+        english: "music"
+      },
+      {
+        content: "초등학교",
+        english: "elementary school"
+      },
+      {
+        content: "중학교",
+        english: "middle school"
+      },
+      {
+        content: "고등학교",
+        english: "high school"
+      },
+      {
+        content: "대학",
+        english: "university"
+      },
+      {
+        content: "공부하다",
+        english: "to study"
+      },
     ]
   },
   "TRAVEL": {
@@ -63,6 +171,34 @@ module.exports = {
         content: "택시",
         english: "taxi"
       },
+      {
+        content: "여행가방",
+        english: "suitcase"
+      },
+      {
+        content: "공항",
+        english: "airport"
+      },
+      {
+        content: "인사동",
+        english: "Insadong"
+      },
+      {
+        content: "인천공항",
+        english: "Incheon International Airport"
+      },
+      {
+        content: "비자",
+        english: "visa"
+      },
+      {
+        content: "여권",
+        english: "passport"
+      },
+      {
+        content: "경찰서",
+        english: "police station"
+      },
     ]
   },
   "COUNTRIES": {
@@ -71,46 +207,118 @@ module.exports = {
     iconClass: "globe",
     challenges: [
       {
-        content: "",
-        english: ""
+        content: "대한민국",
+        english: "South Korea"
       },
       {
-        content: "",
-        english: ""
+        content: "미국",
+        english: "USA"
       },
       {
-        content: "",
-        english: ""
+        content: "중국",
+        english: "China"
       },
       {
-        content: "",
-        english: ""
+        content: "일본",
+        english: "Japan"
       },
       {
-        content: "",
-        english: ""
+        content: "북한",
+        english: "North Korea"
+      },
+      {
+        content: "독일",
+        english: "Germany"
+      },
+      {
+        content: "프랑스",
+        english: "France"
+      },
+      {
+        content: "영국",
+        english: "UK"
+      },
+      {
+        content: "스페인",
+        english: "Spain"
+      },
+      {
+        content: "이탈리아",
+        english: "Italy"
+      },
+      {
+        content: "러시아",
+        english: "Russia"
+      },
+      {
+        content: "인도",
+        english: "India"
+      },
+      {
+        content: "태국",
+        english: "Thailand"
+      },
+      {
+        content: "호주",
+        english: "Australia"
+      },
+      {
+        content: "대만",
+        english: "Taiwan"
+      },
+      {
+        content: "캐나다",
+        english: "Canada"
+      },
+      {
+        content: "멕시코",
+        english: "Mexico"
+      },
+      {
+        content: "페루",
+        english: "Peru"
+      },
+      {
+        content: "브라질",
+        english: "Brazil"
+      },
+      {
+        content: "아르헨티나",
+        english: "Argentina"
+      },
+      {
+        content: "칠레",
+        english: "Chile"
+      },
+      {
+        content: "남아프리카",
+        english: "South Africa"
+      },
+      {
+        content: "폴란드",
+        english: "Poland"
       },
     ]
   },
   "BODY": {
     categoryName: "Body Parts",
-    difficulty: MEDIUM_DIFFICULTY,
+    difficulty: EASY_DIFFICULTY,
     iconClass: "male",
     challenges: [
       {
-        content: "",
+        content: "눈",
         english: "eyes"
       },
       {
-        content: "",
+        content: "눈썹",
         english: "eyebrows"
       },
       {
-        content: "",
+        content: "머리",
         english: "head"
       },
       {
-        content: "",
+        content: "코",
         english: "nose"
       },
       {
@@ -118,20 +326,48 @@ module.exports = {
         english: "cheeks"
       },
       {
-        content: "",
+        content: "입술",
         english: "lips"
       },
       {
-        content: "",
+        content: "귀",
         english: "ears"
       },
       {
-        content: "",
+        content: "턱",
         english: "chin"
       },
       {
-        content: "",
+        content: "목",
         english: "neck"
+      },
+      {
+        content: "팔꿈치",
+        english: "elbow"
+      },
+      {
+        content: "배",
+        english: "belly"
+      },
+      {
+        content: "다리",
+        english: "legs"
+      },
+      {
+        content: "손",
+        english: "hand"
+      },
+      {
+        content: "손가락",
+        english: "fingers"
+      },
+      {
+        content: "발",
+        english: "feet"
+      },
+      {
+        content: "발가락",
+        english: "toes"
       },
     ]
   },
@@ -141,24 +377,64 @@ module.exports = {
     iconClass: "map-marker",
     challenges: [
       {
-        content: "",
+        content: "은행",
         english: "bank"
       },
       {
-        content: "",
+        content: "편의점",
         english: "convenience store"
       },
       {
-        content: "",
+        content: "병원",
         english: "hospital"
       },
       {
-        content: "",
+        content: "백화점",
         english: "department store"
       },
       {
-        content: "",
+        content: "대사관",
         english: "embassy"
+      },
+      {
+        content: "우체국",
+        english: "post office"
+      },
+      {
+        content: "공원",
+        english: "park"
+      },
+      {
+        content: "바닷가",
+        english: "beach"
+      },
+      {
+        content: "제주도",
+        english: "Jeju Island"
+      },
+      {
+        content: "기차역",
+        english: "train station (KTX)"
+      },
+      {
+        content: "지하철역",
+        english: "subway station"
+      },
+      {
+        content: "빵집",
+        english: "bakery"
+      },
+      {
+        content: "화장실",
+        english: "bathroom"
+      },
+      {
+        content: "휴게소",
+        english: "highway rest stop"
+      },
+      {
+        content: "커피숍",
+        english: "coffee shop"
       },
     ]
   },
@@ -168,28 +444,147 @@ module.exports = {
     iconClass: "desktop",
     challenges: [
       {
-        content: "",
+        content: "컴퓨터",
         english: "computer"
       },
       {
-        content: "",
+        content: "로그인",
         english: "login"
       },
       {
-        content: "",
+        content: "패스워드",
         english: "password"
       },
       {
-        content: "",
+        content: "이메일",
         english: "email"
       },
       {
+        content: "해커",
+        english: "hacker"
+      },
+      {
+        content: "주민등록번호",
+        english: "resident registration number"
+      },
+      {
+        content: "구글",
+        english: "google"
+      },
+      {
+        content: "프로그래밍언어",
+        english: "programming language"
+      },
+      {
+        content: "인터넷",
+        english: "internet"
+      },
+      {
+        content: "와이파이",
+        english: "wifi"
+      },
+      {
+        content: "스마트폰",
+        english: "smartphone"
+      },
+      {
+        content: "마우스",
+        english: "mouse"
+      },
+      {
+        content: "스팸",
+        english: "spam"
+      },
+      {
+        content: "피싱",
+        english: "phishing"
+      },
+      {
+        content: "스타크래프트",
+        english: "starcraft"
+      },
+    ]
+  },
+  "VERBS": {
+    categoryName: "Verbs",
+    difficulty: MEDIUM_DIFFICULTY,
+    iconClass: "book",
+    challenges: [
+      {
         content: "",
-        english: "account"
+        english: ""
       },
       {
         content: "",
-        english: "leet hacker"
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
+      },
+      {
+        content: "",
+        english: ""
       },
     ]
   }
