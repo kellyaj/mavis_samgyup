dad3be2e9e10d94c4e2e2b62405588cf4705a13b
Andrew Kelly <kellyaj@gmail.com>
2017-02-05 16:28:42 -0800

    adds keyflash styling

--- src/KeyFlash.js
+++ src/KeyFlash.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react'
 class KeyFlash extends Component {
 
   presentLastKeys() {
-    if(this.props.lastPressedKey) {
+    if(this.props.lastPressedKey == 'Backspace') {
+      return `Backspace`
+    } else if(this.props.lastPressedKey) {
       return `${this.props.lastPressedKorChar} ( ${this.props.lastPressedKey} )`
+    }
+  }
+
+  presentKeyNote() {
+    if(this.props.lastPressedKey == 'Backspace') {
+      return (
+        <div className="key-note-container">
+          NOTE: Backspace will remove an entire Korean character, which may be composed of multiple characters that have been merged. This results in extra english characters in the above input. When in doubt, clear it out.
+        </div>
+      )
     }
   }
 
@@ -13,6 +25,7 @@ class KeyFlash extends Component {
     return (
       <div className="keyflash-container">
         { this.presentLastKeys() }
+        { this.presentKeyNote()}
       </div>
     )
   }
